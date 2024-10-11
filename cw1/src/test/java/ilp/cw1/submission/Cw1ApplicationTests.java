26c96d4a127e1c7e9208ec56950b6a1eb6c75bbb
skyee <skye.emberson@gmail.com>
2024-10-11 16:17:52 +0100

    test

---
+++ cw1/src/test/java/ilp/cw1/submission/Cw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ilp.cw1.submission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
